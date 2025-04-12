e2ec60a70480adfcf5e383770ce469736d607a36
diboti <korposb@gmail.com>
2025-04-12 23:46:41 +0200

    comm working, impl needed

--- app/controllers/RecommendationController.py
+++ app/controllers/RecommendationController.py
@@ -12,14 +12,15 @@ bp = Blueprint('recommendation', __name__, url_prefix='/')
 @bp.route('/recommend', methods=('GET', 'POST'))
 def recommend():
     data = request.get_json()
-    user = data.get('user')
-    if (user is None):
-        return Response('{\n"message":"No user provided"}', status=400, mimetype='application/json')
+    print(data)
+    #user = data.get('user')
+    #if (user is None):
+    #    return Response('{\n"message":"No user provided"}', status=400, mimetype='application/json')
     
-    user_id = user.get('id')
-    liked_games = user.get('liked')
-    if (user_id is None or liked_games is None):
-        return Response('{\n"message":"Invalid user provided"}', status=400, mimetype='application/json')
-    
-    recommended_games = getRecommendation(123)
+    #user_id = user.get('id')
+    #liked_games = user.get('liked')
+    #if (user_id is None or liked_games is None):
+    #    return Response('{\n"message":"Invalid user provided"}', status=400, mimetype='application/json')
+
+    recommended_games = getRecommendation(1,1)
     return recommended_games
--- app/service/RecommendationService.py
+++ app/service/RecommendationService.py
@@ -2,9 +2,9 @@ from flask import current_app
 import requests
 
 
-def getRecommendation(liked_games):
+def getRecommendation(liked_games, token):
     base_url = current_app.config["MAIN_SERVER_API_BASE_URL"]
-    response = requests.get(f"{base_url}/igdb/popular")
+    response = requests.get(f"{base_url}/igdb/liked-games")
     print(response.json())
 
     return response.json()
